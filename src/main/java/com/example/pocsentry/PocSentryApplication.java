f9aa5054b907d7d209743ad5742a056ac801eda9
Patrick Scaramal <p.scaramal@reply.com>
2023-05-24 15:42:43 -0300

    commit inicial

---
+++ src/main/java/com/example/pocsentry/PocSentryApplication.java
@@ -0,0 +1,13 @@
+package com.example.pocsentry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSentryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSentryApplication.class, args);
+	}
+
+}
